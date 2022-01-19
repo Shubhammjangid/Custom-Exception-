6f3902149961d843f9e20ef777756a9af1321e6d
Shubhammjangid <shubham.jangid@netsolutions.com>
2022-01-19 16:51:30 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Exe{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+         int x, y, z;
+            Console.WriteLine("Enter number 1 ?");
+            x  = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter number 2 ?");
+            y = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                if (y % 2 > 0)
+                {
+                  
+                    throw new OddNumberException();
+                }
+                z = x / y;
+                Console.WriteLine(z);
+            }
+            catch (OddNumberException one)
+            {
+                Console.WriteLine(one.Message);
+            }
+            Console.WriteLine("End of the program");
+         
+          
+        }
+    }
+    public class OddNumberException : Exception
+    {
+      
+        public override string Message
+        {
+            get
+            {
+                return "divisor cannot be odd number";
+            }
+        }
+    }
+
+}
+       
+ 
